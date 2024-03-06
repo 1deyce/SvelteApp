98783d71107477c47516c16e2189d875e0e3dc85
1deyce <124916049+1deyce@users.noreply.github.com>
2024-03-06 12:06:07 +0200

    New
    - Product Card creation
    - Hero completion
    - Assets added (3)

--- src/components/SectionWrapper.svelte
+++ src/components/SectionWrapper.svelte
@@ -1,9 +1,9 @@
-<script>
-    export let id;
+<script lang="ts">
+    export let id: string;
 
 </script>
 
-<section {id} class={"min-h-screen flex flex-col px-4"}>
+<section id={id} class={"min-h-screen flex flex-col px-4 "}>
     <div class="flex flex-col flex-1 max-w-[1400px] mx-auto w-full">
         <slot/>
     </div>
